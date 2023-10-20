928942a25368a56c58f740ce2edbba861aaa59e7
GodUltraMa <godultramax@gmail.com>
2023-10-20 11:26:45 +0530

    portfolio V2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,9 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes ,{
-    scrollPositionRestoration: 'top', // Set the scroll position strategy
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
